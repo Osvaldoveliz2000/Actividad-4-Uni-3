7f9ef0b34e45240f9f9f26fec2b4297f27bf69f9
Osvaldoveliz2000 <125836784+Osvaldoveliz2000@users.noreply.github.com>
2023-03-09 23:39:14 -0600

    Add files via upload

---
+++ Actividad 4/prueva 2/prueva 2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prueva_2
+{
+    public partial class Form1 : Form 
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Operaciones2 x = new Operaciones2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+            Operaciones2 oper = new Operaciones2();
+            double resultado = oper.Sumar(a, b);
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(txtA.Text);
+            double b = Convert.ToDouble(txtB.Text);
+            Operaciones2 oper = new Operaciones2();
+            double resultado = oper.Restar(a, b);
+            txtResultado.Text = resultado.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
